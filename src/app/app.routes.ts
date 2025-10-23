73078d2ff075b54c60c3d9101f990d74b9728fa6
Allwyn46 <allwyn153@gmail.com>
2025-10-23 17:52:56 +0530

    Added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/auth/login/login';
+import { Register } from './components/auth/register/register';
+import { Layout } from './components/layout/layout';
+import { Dashboard } from './components/pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'register',
+    component: Register,
+  },
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: 'dashboard',
+        component: Dashboard,
+      },
+    ],
+  },
+];
