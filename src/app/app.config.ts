2d940c525a10a243a98ec5bc4162bdfb16b302c6
Allwyn46 <allwyn153@gmail.com>
2025-10-23 19:42:19 +0530

    Added proxy settings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,12 +8,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { EVENT_MANAGER_PLUGINS } from '@angular/platform-browser';
 import { PreventDefaultEventPlugin } from './services/prevent-default-event';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
     {
       provide: EVENT_MANAGER_PLUGINS,
       multi: true,
--- src/app/components/auth/login/login.ts
+++ src/app/components/auth/login/login.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { ZardCardComponent } from 'y/card/card.component';
 import { ZardButtonComponent } from 'y/button/button.component';
 import { RouterLink } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoginFormat } from 'src/app/models/quo.model';
 
 @Component({
   selector: 'app-login',
@@ -13,4 +14,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 export class Login {
   idEmail = '';
   idPassword = '';
+
+  loginform: FormGroup = new FormGroup<LoginFormat>({
+    username: new FormControl('',{nonNullable:true,validators:[Validators.required,Validators.email]),
+    password: new FormControl('',{nonNullable:true,validators:[Validators.required]})
+  });
+
+  onlogin(){
+
+  }
 }
