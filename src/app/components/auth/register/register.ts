56df934d380bd5f915f2e3c6bd307d101488ad15
Allwyn46 <allwyn153@gmail.com>
2025-10-24 12:49:49 +0530

    completed register

--- src/app/components/auth/register/register.ts
+++ src/app/components/auth/register/register.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { RegisterFormat } from 'src/app/models/quo.model';
+import { ZardButtonComponent } from 'y/button/button.component';
+import { ZardCardComponent } from 'y/card/card.component';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ZardCardComponent, ZardButtonComponent, RouterLink, ReactiveFormsModule],
   templateUrl: './register.html',
-  styleUrl: './register.css'
+  styleUrl: './register.css',
 })
 export class Register {
+  registerForm: FormGroup = new FormGroup<RegisterFormat>({
+    username: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    confirmPassword: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    ismfaactive: new FormControl(false, {
+      nonNullable: true,
+    }),
+  });
 
+  onRegister() {}
 }
--- src/app/models/quo.model.ts
+++ src/app/models/quo.model.ts
@@ -4,3 +4,10 @@ export interface LoginFormat {
   username: FormControl<string>;
   password: FormControl<string>;
 }
+
+export interface RegisterFormat {
+  username: FormControl<string>;
+  password: FormControl<string>;
+  confirmPassword: FormControl<string>;
+  ismfaactive: FormControl<boolean>;
+}
